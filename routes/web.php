e52a88efa8421913a1fb025c47d5aa50d669601e
plorenpurba <plorentinpurba1@gmail.com>
2026-07-20 20:24:09 +0700

    menambahkan halaman gudang

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GudangController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,6 @@ Route::get('/register', [AuthController::class, 'register'])->name('register');
 
 // Dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Gudang & Stok (Ditambahkan agar halaman gudang.blade.php bisa diakses)
+Route::get('/gudang', [GudangController::class, 'index'])->name('gudang.index');
